a64d57ab61d81d0b8fd0337847922ba6712ea7ff
EiKaSo <kathrynsortland@gmail.com>
2025-01-09 14:23:08 +0100

    interactive ham-menu fuction

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const hamMenu = document.querySelector('ham-menu');
+document.addEventListener("DOMContentLoaded", () => {
+    const hamMenu = document.querySelector(".ham-menu");
 
-const offScreenMenu = document.querySelector('.off-screen-menu');
+    const offScreenMenu = document.querySelector(".off-screen-menu");
 
-hamMenu.addEventListener('click',()=>{
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
+    hamMenu.addEventListener("click", () => {
+        hamMenu.classList.toggle("active");
+        offScreenMenu.classList.toggle("active");
+    })
 })
